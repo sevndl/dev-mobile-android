8e4e357b035ce0a9f4f80c26b69da0a54774190b
sevndl <nandillons@gmail.com>
2021-02-18 13:10:00 +0100

    Récupération des rayons depuis le webservice
    TODO -> trouver pourquoi le nom ne s'affiche pas à la place de "title"

--- app/src/main/java/fr/epsi/shop/CategoriesActivity.java
+++ app/src/main/java/fr/epsi/shop/CategoriesActivity.java
@@ -2,8 +2,20 @@ package fr.epsi.shop;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class CategoriesActivity extends EpsiActivity {
+
+    private ArrayList<Categories> categories;
 
     public static void displayActivity(EpsiActivity activity, String title) {
         Intent intent = new Intent(activity, CategoriesActivity.class);
@@ -18,5 +30,35 @@ public class CategoriesActivity extends EpsiActivity {
         String title = getIntent().getExtras().getString("title","");
         showBack();
         setTitle(title);
+
+        categories = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewCategories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CategoriesAdapter categoriesAdapter = new CategoriesAdapter(this, categories);
+        recyclerView.setAdapter(categoriesAdapter);
+
+        String url = "https://djemam.com/epsi/categories.json";
+        WSCall wsCall = new WSCall(url, new WSCall.Callback() {
+            @Override
+            public void onComplete(String result) {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray jsonArray = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        Categories categorie = new Categories(jsonArray.getJSONObject(i));
+                        categories.add(categorie);
+                    }
+                    categoriesAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Toast.makeText(CategoriesActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+        wsCall.run();
     }
 }
---
+++ app/src/main/java/fr/epsi/shop/WSCall.java
@@ -0,0 +1,89 @@
+package fr.epsi.shop;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class WSCall {
+    public interface Callback {
+        void onComplete(String result);
+        void onError(Exception e);
+    }
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private Callback callback;
+    private String wsUrl;
+
+    public WSCall(String wsUrl, Callback callback) {
+        this.wsUrl = wsUrl;
+        this.callback = callback;
+    }
+
+    public void run() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream inputStream = null;
+                    URL url = new URL(wsUrl);
+                    HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+                    httpURLConnection.setDefaultUseCaches(false);
+                    httpURLConnection.connect();
+                    inputStream = httpURLConnection.getInputStream();
+                    postRun(convertStreamToString(inputStream));
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                    postRun(e);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    postRun(e);
+                }
+            }
+        });
+    }
+
+    private void postRun(Object o){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (o instanceof Exception) {
+                    callback.onError((Exception) o);
+                } else {
+                    callback.onComplete(o.toString());
+                }
+            }
+        });
+    }
+
+    private static String convertStreamToString(InputStream is){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+            StringBuilder stringBuffer = new StringBuilder("");
+            String line;
+
+            String NL = System.getProperty("line.separator");
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                stringBuffer.append(line).append(NL);
+            }
+            bufferedReader.close();
+            return stringBuffer.toString();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
