7a8e2a995f0ee55f8d6a33336ac80323eb4fd610
sevndl <nandillons@gmail.com>
2021-02-21 15:21:39 +0100

    Récupération de la photo en fonction du click

--- app/src/main/java/fr/epsi/shop/ProfileActivity.java
+++ app/src/main/java/fr/epsi/shop/ProfileActivity.java
@@ -1,10 +1,7 @@
 package fr.epsi.shop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 public class ProfileActivity extends EpsiActivity {
@@ -12,6 +9,7 @@ public class ProfileActivity extends EpsiActivity {
     public static void displayActivity(EpsiActivity activity, String title, String photo) {
         Intent intent = new Intent(activity, ProfileActivity.class);
         intent.putExtra("title", title);
+        intent.putExtra("photo", photo);
         activity.startActivity(intent);
     }
 
@@ -21,12 +19,12 @@ public class ProfileActivity extends EpsiActivity {
         setContentView(R.layout.activity_profile);
 
         String title = getIntent().getExtras().getString("title","");
+        String photo = getIntent().getExtras().getString("photo","");
         showBack();
         setTitle(title);
 
         ImageView imageView = findViewById(R.id.photo);
-
-
-
+        int id_photo = getResources().getIdentifier(photo, "drawable", getPackageName());
+        imageView.setImageResource(id_photo);
     }
 }
